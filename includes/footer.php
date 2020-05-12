c95d7e036d5e90b3de441ff775d5eb6f21263560
root <root@ip-172-31-34-48.ec2.internal>
2020-05-12 11:40:26 +0000

    Removed sensative info

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+
+		<!--FOOTER-->	
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 align-center">
+						<ul class="legals">
+							<li><button class="md-trigger " data-modal="modal-11">2020</button></li>
+							<li><a href="">Ajmain Naqib</a></li>
+						</ul>
+					</div>
+				
+				</div>
+			</div>
+        </footer>
+		<!-- /FOOTER -->
+		<!--SCRIPTS-->	
+		
+		<script type="text/javascript" src="assets/js/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="assets/js/jquery-ui-1.10.4.min.js"></script>
+ 
+		<script type="text/javascript" src="assets/js/classie.js" ></script>
+		<script type="text/javascript" src="assets/js/modalEffects.js" ></script>
+
+        <script type="text/javascript" src="assets/js/bootstrap.min.js" ></script>
+        
+		<!-- TEAM SLIDER  -->
+		<script type="text/javascript" src="assets/js/jquery.flexslider.js" ></script>
+		
+
+		<!-- SMOOTH SCROLL  -->
+		<script type="text/javascript" src="assets/js/smooth-scroll.js"></script>
+		
+		<!-- NICE SCROLL  -->
+		<!-- <script type="text/javascript" src="assets/js/jquery.nicescroll.js"></script> -->
+		
+		<!-- ANIMATION  -->
+		<script type="text/javascript" src="assets/js/wow.min.js"></script>
+		
+
+		<!-- SUBSCRIPTION FORM PLACEHOLDER -->
+		<script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+
+
+        <script type="text/javascript" src="assets/js/modernizr.js" ></script>
+
